69e98403cc71b942e48e104a6500cafa33ec17ba
Andrey Oleshko <Dweren.Iaoeo@mail.ru>
2022-01-30 14:04:03 +0200

    Sell Elements by amount and with Science

---
+++ test24/Assets/Scripts/DwScripts/SliderAmountToSell.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderAmountToSell : MonoBehaviour
+{
+    private Slider sliderAmount;
+    public Text textValue;
+
+    private void Awake()
+    {
+        sliderAmount = GetComponent<Slider>();
+        textValue.text = sliderAmount.value.ToString();
+    }
+
+    public void ShowSliderValue()
+    {
+        textValue.text = sliderAmount.value.ToString();
+    }
+
+    public void ChangeSelect(int maxValue)
+    {
+        sliderAmount.minValue = 1;
+        sliderAmount.maxValue = maxValue;
+        sliderAmount.value = 1;
+    }
+
+    public int GetSliderValue()
+    {
+        return (int)sliderAmount.value;
+    }
+}
