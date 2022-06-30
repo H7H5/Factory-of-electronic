51a12f989c40c0697568e9fe0495b8f7ee8205d0
Circle_java_ATB <Serdyk87@ukr.net>
2022-06-30 20:06:00 +0300

    software construction of upgrade elements

--- test24/Assets/Scripts/BoScripts/Uprgade/UpGradeBilder.cs
+++ test24/Assets/Scripts/BoScripts/Uprgade/UpGradeBilder.cs
@@ -6,6 +6,9 @@ public class UpGradeBilder : MonoBehaviour
     public static UpGradeBilder Instance;
     [SerializeField] private List<GameObject> ups = new List<GameObject>();
     [SerializeField] private GameObject panelNeedResourse;
+    [SerializeField] private GameObject content1;
+    [SerializeField] private GameObject prefUP;
+
     private void Awake()
     {
         if (Instance == null)
@@ -15,6 +18,15 @@ public class UpGradeBilder : MonoBehaviour
     }
     public void Bildding(List<int> arrayOnElements)
     {
+        if (ups.Count == 0)
+        {
+            for (int j = 0; j < DBase.Instance.elementsParameters.Length; j++)
+            {
+                GameObject up = Instantiate(prefUP, content1.transform);
+                up.GetComponent<ItemUpgrade>().Init(DBase.Instance.elementsParameters[j]);
+                ups.Add(up);
+            }
+        }
         for (int i = 0; i < ups.Count; i++)
         {
             for (int j = 0; j < arrayOnElements.Count; j++)
