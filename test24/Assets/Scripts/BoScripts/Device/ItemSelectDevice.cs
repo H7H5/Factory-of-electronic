a827e2edad0ad41b2cedf2279567a3ad3bbc9bbf
Circle_java_ATB <Serdyk87@ukr.net>
2022-09-10 01:36:29 +0300

    optimization select bild devices

--- test24/Assets/Scripts/BoScripts/Device/ItemSelectDevice.cs
+++ test24/Assets/Scripts/BoScripts/Device/ItemSelectDevice.cs
@@ -6,22 +6,13 @@ public class ItemSelectDevice : MonoBehaviour
 {
     public int id;
     [SerializeField] private Button button;
-    [SerializeField] private List<GameObject> devices = new List<GameObject>();
     public void SetImage(Sprite sprite)
     {
             button.GetComponent<Image>().sprite = sprite;
     }
     public void OnClick()
     {
-        GameObject currentDevice = devices[0];
-        for (int i = 0; i < devices.Count; i++)
-        {
-            if (devices[i].GetComponent<ItemDevice>().device.id==id)
-            {
-                currentDevice = devices[i];
-            }
-        }
-        DBase.Instance.SetSaverDevice(currentDevice);
+        DBase.Instance.SetSaverDevice(DBase.Instance.getDeviceObj(id));
         DBase.Instance.DeviceSelect.SetActive(false);
         ButtonController.Instance.UnBlockButtons();
         DBase.Instance.OpenSchema.GetComponent<SchemButtonHelper>().OpenSchema();
