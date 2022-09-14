825cc0f43244dce92d868e54616b69919b0a295a
Circle_java_ATB <Serdyk87@ukr.net>
2022-09-14 19:55:31 +0300

    meat grinder

--- test24/Assets/Scripts/BoScripts/Save/Devices.cs
+++ test24/Assets/Scripts/BoScripts/Save/Devices.cs
@@ -74,5 +74,6 @@ public class Devices
         Insert_device(unitySQLite, 6, 0, "curling iron");
         Insert_device(unitySQLite, 7, 0, "sampler");
         Insert_device(unitySQLite, 8, 0, "blender");
+        Insert_device(unitySQLite, 9, 0, "meat grinder");
     }
 }
