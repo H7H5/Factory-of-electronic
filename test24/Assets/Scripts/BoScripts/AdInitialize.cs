83d27e73c1d94a6adc2c1ac229155496d13db5f8
Circle_java_ATB <Serdyk87@ukr.net>
2022-10-13 19:43:05 +0300

    AdMob

---
+++ test24/Assets/Scripts/BoScripts/AdInitialize.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleMobileAds.Api;
+
+public class AdInitialize : MonoBehaviour
+{
+    private InterstitialAd interstitialAd;
+    private string openUnitId = "ca-app-pub-3940256099942544/3419835294";
+    private void Awake()
+    {
+        MobileAds.Initialize(initStatus => { });
+        interstitialAd = new InterstitialAd(openUnitId);
+        AdRequest adRequest = new AdRequest.Builder().Build();
+        interstitialAd.LoadAd(adRequest);
+        ShowAd();
+    }
+    private void OnEnable()
+    {
+        
+    }
+    public void ShowAd()
+    {
+        if (interstitialAd.IsLoaded())
+        {
+            interstitialAd.Show();
+        }
+    }
+}
